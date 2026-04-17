42b388b82cebe0af533812be9c9ad6adc4d7099f
Sergi Gallart Fanlo <132823053+Sgallartfanlo@users.noreply.github.com>
2026-04-17 13:58:01 +0200

    Delete Monitorització de xarxes (Zabbix, Cacti, Nagios)/Scripts_utilitzats directory

--- Monitorització de xarxes (Zabbix, Cacti, Nagios)/Scripts_utilitzats/script1_logs.ps1
+++
@@ -1,5 +0,0 @@
-(Get-WinEvent -FilterHashtable @{
-    LogName = 'Security'
-    ID = 4625 #editar el id según su necesidad.
-} | Sort-Object TimeCreated -Descending | Select-Object -First 1).Message
-
